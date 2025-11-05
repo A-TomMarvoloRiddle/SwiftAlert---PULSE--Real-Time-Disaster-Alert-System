a7069eb05cacf76bd3605feb96460c5e09cea8cc
Apaar Mathur <ap1203m@gmail.com>
2025-11-05 20:27:47 +0000

    Hide API Key

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-7018621954-d4888",
   "appId": "1:579441470593:web:f3b5f61111eb4286e93180",
-  "apiKey": "AIzaSyAOrGhMflRTAPXO7X9M5ASQcJwBbMde6W4",
+  "apiKey": "###",
   "authDomain": "studio-7018621954-d4888.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "579441470593"
