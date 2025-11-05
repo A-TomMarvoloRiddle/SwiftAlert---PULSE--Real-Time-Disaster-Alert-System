0066d85dd5e5d5607f7acddc7c139bba7d068d49
Apaar Mathur <ap1203m@gmail.com>
2025-11-05 20:18:19 +0000

    Remove API Key routes of genkit-ai

--- src/app/api/genkit/route.ts
+++
@@ -1,9 +0,0 @@
-
-import {createNextApiHandler} from '@genkit-ai/next';
-
-// Import flows to register them with Genkit
-import '@/ai/flows/automated-disaster-insights';
-import '@/ai/flows/fetch-disaster-data';
-import '@/ai/flows/translate-text';
-
-export const {GET, POST} = createNextApiHandler();
